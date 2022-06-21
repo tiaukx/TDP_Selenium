f342e821e37a7f287fad8248079e0d7115ed1972
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-21 16:52:28 +0100

    Update testGoogle.spec.js

--- testGoogle.spec.js
+++ testGoogle.spec.js
@@ -26,28 +26,36 @@ describe("Check the search functionality in Google", function() {
         await driver.quit();
     });
 
-    //where we do the test itself
-    it("Go to google and search for QA", async () => {
-        //first we grab the URL of the page we are testing
-        driver.get("http://www.google.co.uk");
-        //need to click agree on a pop up, so we inspect the element on the browser to find the id / class of the button we want it to click
-        //accepts cookies pop up, that appears on each new browser
-        driver.findElement(By.id("L2AGLb")).click();
-        //now we need to access the search bar, once again, inspect the element via browser to get id / class / name of element
-        //await used to tell it to wait until our input has fully been entered
-        await driver.findElement(By.name("q")).sendKeys("QA", Key.ENTER);
-        //checks the title of the page to check the result is what we expect
-        const val = await driver.wait(driver.getTitle(), 1000);
-        //checks if the value we got is equal to what we expected
-        assert.equal(val, "QA - Google Search")
-    });
+    // //where we do the test itself
+    // it("Go to google and search for QA", async () => {
+    //     //first we grab the URL of the page we are testing
+    //     driver.get("http://www.google.co.uk");
+    //     //need to click agree on a pop up, so we inspect the element on the browser to find the id / class of the button we want it to click
+    //     //accepts cookies pop up, that appears on each new browser
+    //     driver.findElement(By.id("L2AGLb")).click();
+    //     //now we need to access the search bar, once again, inspect the element via browser to get id / class / name of element
+    //     //await used to tell it to wait until our input has fully been entered
+    //     await driver.findElement(By.name("q")).sendKeys("QA", Key.ENTER);
+    //     //checks the title of the page to check the result is what we expect
+    //     const val = await driver.wait(driver.getTitle(), 1000);
+    //     //checks if the value we got is equal to what we expected
+    //     assert.equal(val, "QA - Google Search")
+    // });
 
-    it("Go to google and search for BMW", async () => {
+    // it("Go to google and search for BMW", async () => {
+    //     driver.get("http://www.google.co.uk");
+    //     driver.findElement(By.id("L2AGLb")).click();
+    //     await driver.findElement(By.name("q")).sendKeys("BMW", Key.ENTER);
+    //     const val = await driver.wait(driver.getTitle(), 1000);
+    //     assert.equal(val, "BMW - Google Search")
+    // });
+
+    it("After searching QA, check searchbox still contains a value of 'QA'", async () => {
         driver.get("http://www.google.co.uk");
         driver.findElement(By.id("L2AGLb")).click();
-        await driver.findElement(By.name("q")).sendKeys("BMW", Key.ENTER);
-        const val = await driver.wait(driver.getTitle(), 1000);
-        assert.equal(val, "BMW - Google Search")
+        await driver.findElement(By.name("q")).sendKeys("QA", Key.ENTER);
+        const val = driver.findElement(By.xpath("//res[text()='QA']"));
+        assert.equal(val, "QA");
     });
 
 });
