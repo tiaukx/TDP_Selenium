03a27a745756a2635fcf002c40fae539dd41ccbf
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-06-22 15:11:59 +0100

    Update testAutomation.spec.js

--- testAutomation.spec.js
+++ testAutomation.spec.js
@@ -3,19 +3,21 @@ const assert = require("assert");
 const { isTypedArray } = require("util/types");
 
 describe("Check the search functionality in Google", function() {
-    this.timeout(30000);
+    this.timeout(80000);
     
     let driver;
 
     beforeEach(async () => {
         driver = await new Builder().forBrowser("chrome").build();
+        driver.manage().setTimeouts({implicit:10000});
     });
 
-    afterEach(async () => {
-        await driver.quit();
-    });
+    // afterEach(async () => {
+    //     await driver.quit();
+    // });
 
     it("Click tshirt button and click on tshirt", async () => {
+         
         driver.get("http://automationpractice.com/index.php");
 
         let product;
@@ -26,18 +28,27 @@ describe("Check the search functionality in Google", function() {
         //click first t shirt product that loads
         await driver.findElement(By.className("button lnk_view btn btn-default")).click();
         
+        //gets the products H1 tag
+        product = await driver.findElement(By.xpath('/html/body/div/div[2]/div/div[3]/div/div/div/div[3]/h1'));
+        //stores H1 tag in text
+        productText = await driver.wait(product.getText(), 1000);
 
-        //gets the product 
-        product = await driver.findElement(By.tagName("h1")).getText();
 
-        
         //clicks add to cart
         await driver.findElement(By.name("Submit")).click();
         //on pop up that appears, clicks proceed to checkout
+
+
         await driver.findElement(By.className("btn btn-default button button-medium")).click();
+
+
+        let basket;
+        let basketText;
         //stores product name in checkout to variable
-        let basket = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a")).getText();
+        basket = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div[2]/table/tbody/tr/td[2]/p/a"));
+        basketText = await basket.getText();
+
         //check that basket and product matches
-        assert.equal(basket, product);
+        assert.equal(basketText, productText);
     });
 });
